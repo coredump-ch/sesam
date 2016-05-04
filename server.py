2955efa0b0832af0d89a3e5da3b1e85e5de4a22b
Danilo Bargen <mail@dbrgn.ch>
2016-05-05 00:48:58 +0200

    Implement AJAX opening

--- server.py
+++ server.py
@@ -1,19 +1,43 @@
+import os
+import subprocess
+
 from flask import Flask, request, render_template
 
 
 app = Flask(__name__)
 
+APP_ROOT = os.environ.get('UUID', 'test')
 
-@app.route('/', methods=['GET'])
+
+@app.route('/%s/' % APP_ROOT, methods=['GET'])
 def main():
     return render_template('main.html')
 
 
-@app.route('/', methods=['POST'])
+@app.route('/%s/' % APP_ROOT, methods=['POST'])
 def open():
     email = request.form['email']
     password = request.form['password']
-    return 'OPENING for %s / %s' % (email, password)
+    if check_credentials(email, password):
+        trigger()
+        return 'Opened', 204
+    else:
+        return 'Bad credentials', 401
+
+
+def check_credentials(email, password):
+    # TODO: Check credentials
+    return True
+
+
+def trigger():
+    """
+    Trigger the open door binary.
+
+    This call will block for a few seconds.
+    """
+    opendoor_path = os.environ.get('OPENDOOR_PATH', 'opendoor')
+    subprocess.check_call(opendoor_path)
 
 
 if __name__ == "__main__":
